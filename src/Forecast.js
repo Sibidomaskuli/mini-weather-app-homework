45ed5bdcd85b679dfff6b7a2293afd4c368dfcbb
Sibidomaskuli <shildrisno1@gmail.com>
2021-04-24 21:27:19 +0100

    Added 2 more columns

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,10 +1,12 @@
-import React from "react"; 
+import React from "react";
+import "./Forecast.css";
 
 export default function Forecast(){
     return (
      <div className="card">
       <div className="card-body">
-       <div className="col-2">
+       <div className="row justify-contents-around">
+       <div className="col">
         <img
         src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
         >
@@ -14,6 +16,14 @@ export default function Forecast(){
         <span className="Units">
          °C |°F
         </span>
+        <div className="col">
+         <ul>
+          <li>Precipitation: 1%</li>
+          <li>Humidity: 34%</li>
+          <li>Wind: 16 km/h</li>
+         </ul>
+        </div>
+       </div>
        </div>
       </div>
      </div>
