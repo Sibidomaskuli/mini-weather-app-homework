5ed7232f87fcf52292239716b3e916c5458483ac
Sibidomaskuli <shildrisno1@gmail.com>
2021-04-24 17:43:04 +0100

    Created Forecast.js file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react"; 
+import Forecast from "./Forecast";
 import './App.css';
 
 export default function App() {
@@ -6,6 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container"> 
       <h2>Weather Search App</h2>
+      <Forecast />
       <footer>
         Made by <em>Shirin Eldris</em> 🤖 and is <a
           href="https://github.com/Sibidomaskuli/mini-weather-app-homework"
---
+++ src/Forecast.js
@@ -0,0 +1,9 @@
+import React from "react"; 
+
+export default function Forecast(){
+    return (
+        <div>
+            Salutations from Forecast
+        </div>
+    )
+}
