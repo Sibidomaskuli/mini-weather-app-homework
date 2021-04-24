7356edb14de505dec7929c7b9612412ed53aa25b
Sibidomaskuli <shildrisno1@gmail.com>
2021-04-24 15:47:59 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        Made by <em>Shirin Eldris</em> 🤖 and is <a
+          href="https://github.com/Sibidomaskuli/mini-weather-app-homework"
+          target="-blank"
+          >
+            open-sourced on GitHub
+        </a>
+        </footer>
     </div>
   );
 }
