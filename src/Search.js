c44be373e475aa5093419a8bac3b1185d72cf87a
Sibidomaskuli <shildrisno1@gmail.com>
2021-04-25 14:59:19 +0100

    Made API call for lat and lon coords

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,25 @@
 import React from "react";
+import axios from "axios";
 import "./Search.css";
 
-export default function Search () {
+export default function Search (){
+ function displayCoordinates ( response ) {
+  console.log( response.data );
+  console.log( response.data[ 0 ].lat );
+  console.log( response.data[ 0 ].lon );
+ }
+ const apiKey = "61de322b4c57e14ee5306e572d84bdb5";
+ let city = "London, UK";
+ let url = `https://api.openweathermap.org/geo/1.0/direct?q=${ city }&limit=5&appid=${ apiKey }&units=metric`;
+ axios.get( url ).then( displayCoordinates );
+
  return (
-  <div className="Search">
+   <div className="Search">
+
+
+    
+
+
    <form>
     <div className="row">
      <div className="col-10">
