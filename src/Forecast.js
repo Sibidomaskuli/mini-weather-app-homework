15e6d64bbf70e6b37926bfe51b6a77faf851a3fb
Sibidomaskuli <shildrisno1@gmail.com>
2021-04-26 23:53:41 +0100

    Updated URL for weather icon

--- src/Forecast.js
+++ src/Forecast.js
@@ -28,37 +28,37 @@ export default function Forecast (props){
     city: response.data.name,
     description: response.data.weather[0].description,
     date: new Date(response.data.dt*1000),
-    iconUrl: "https://ss1.gstatic.com/onebox/weather/64/partly_cloudy.png",   
+    iconUrl: "https://openweathermap.org/img/wn/10d@2x.png",   
    });
   }
   
- return (
-  <div className="card">
-   <div className="card-body">
-    <h3>Weather Search App</h3>
-    <form onSubmit={handleSubmit}>
-     <div className="row">
-      <div className="col-10">
-       <input
-        type="search"
-        placeholder="Enter a city.."
-        className="form-control"
-        autoFocus="on"
-        onChange={updateCity}
-       />
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h3>Weather Search App</h3>
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-10">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={updateCity}
+              />
+            </div>
+            <div className="col-2">
+              <input type="submit"
+                value="Search"
+                className="btn btn-success w-100" />
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
       </div>
-      <div className="col-2">
-       <input type="submit"
-        value="Search"
-        className="btn btn-success w-100" />
-      </div>
-     </div>
-    </form>
-    <WeatherInfo data={weatherData}/>
-   </div>
-   <Footer />
-  </div>
- );
+      <Footer />
+    </div>
+  );
 }
 
             
