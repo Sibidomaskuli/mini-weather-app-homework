b8df44197230398e49094a10e9c62412efc7fcd2
Sibidomaskuli <shildrisno1@gmail.com>
2021-04-27 19:42:29 +0100

    Changed default city

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Forecast defaultCity="Kuwait" />        
+        <Forecast defaultCity="Cairo" />        
       </div>
     </div>
   );
