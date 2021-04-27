5e9aea329b56ec3dbbb1f9b74f97741e086f2a29
Sibidomaskuli <shildrisno1@gmail.com>
2021-04-27 18:35:46 +0100

    Cleaned code in Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -2,25 +2,23 @@ import React from "react";
 import "./Footer.css"; 
 
 export default function Footer (){
-  return (
-    <div className="Footer">
-      <footer>
-          Brain- stormed by <em>Shirin Eldris</em> 🤖, { "" }
-          <a
-            href="https://github.com/Sibidomaskuli/mini-weather-app-homework"
-            className="purple"
-            target="-blank"
-            rel="noopener noreferrer"
-          >
-            open-sourced on GitHub,
-                </a>
-          <a href="#"            
-            className="green"
-            target="_blank"
-            rel="noopener noreferrer"            
-          > Netlify hosted
-          </a>
-        </footer>
-    </div>
-  );
+ return (
+  <div className="Footer">
+   <footer>
+    Brain- stormed by <em>Shirin Eldris</em> 🤖, {""}
+    <a
+     href="https://github.com/Sibidomaskuli/mini-weather-app-homework"
+     className="purple"
+     target="_blank"
+     rel="noopener noreferrer"
+    >
+     Open-Sourced on GitHub,
+    </a>
+    <a href="/" className="green" target="_blank" rel="noopener noreferrer">
+     {" "}
+     Netlify hosted
+    </a>
+   </footer>
+  </div>
+ );
 }
