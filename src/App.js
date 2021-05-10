b554c7cad571716df69c65d0c5f3fbeed3b3eaeb
Sibidomaskuli <shildrisno1@gmail.com>
2021-05-10 04:04:42 +0100

    Changed background image and default city

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Forecast defaultCity="Cairo" /> 
+        <Forecast defaultCity="Dubai" /> 
       </div>
     </div>
   );
--- src/Spare.js
+++ src/Spare.js
@@ -1,12 +1,29 @@
+
+// Always copy image address not link address 
+
+// This makes any gif cover entire screen without repeating
+body{
+  background-image: url('http://i.stack.imgur.com/kx8MT.gif');
+  background-size: cover;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+}
+
+// palm tree overlooking golden sandy beach
+background-image: url("https://cdn.wallpapersafari.com/94/92/L3cw9K.jpeg");
+
+// bunch of palm trees in the desert
+background-image: url("https://cdn.wallpapersafari.com/75/35/1LE4vI.jpg");  
+  
 background-image: url("https://inews-prd-a-images.s3.eu-west-2.amazonaws.com/content/uploads/2019/10/shutterstock_1479789158.jpg");
 
-background-color: #e4fbff;
 
 background-image: url("https://wallpaperaccess.com/full/371818.jpg");
 
 //Toroto playing a trumpet, teal background: url("https://i.pinimg.com/originals/12/25/8a/12258aa80d5bbe28120b77537dcf8668.png");
+background-image: url("https://i.pinimg.com/originals/12/25/8a/12258aa80d5bbe28120b77537dcf8668.png");  
 
-// Always copy image address not link address 
 
 body {
   font-family: "Yantramanav", sans-serif;
@@ -18,7 +35,5 @@ body {
   background-blend-mode: darken;
  }
 
-
-.container {
-  max-width: 800px;
-}
+//animated Tokyo background cartoon repeated 4 times  
+background-image: url("https://i.pinimg.com/originals/3d/0f/d8/3d0fd842be0ccdaf97d960312c7b34e6.gif");
