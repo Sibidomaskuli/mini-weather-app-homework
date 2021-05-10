5045072437f1e9e2305acfbaab9919329ba64ecd
Sibidomaskuli <shildrisno1@gmail.com>
2021-05-10 02:04:23 +0100

    Added background image

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
       <div className="container">
         <Forecast defaultCity="Cairo" /> 
       </div>
-      <div style={{backgroundImage: `url("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F852658141930745508%2F&psig=AOvVaw065_wdJAd8EYifwNEOD2QZ&ust=1619639357040000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLDnpfaYn_ACFQAAAAAdAAAAABAD")`}}></div>
     </div>
   );
 }
+      
---
+++ src/Spare.js
@@ -0,0 +1,10 @@
+background-image: url("https://inews-prd-a-images.s3.eu-west-2.amazonaws.com/content/uploads/2019/10/shutterstock_1479789158.jpg");
+
+background-color: #e4fbff;
+
+background-image: url("https://wallpaperaccess.com/full/371818.jpg");
+
+//Toroto playing a trumpet, teal background: url("https://i.pinimg.com/originals/12/25/8a/12258aa80d5bbe28120b77537dcf8668.png");
+
+// Always copy image address not link address 
+
